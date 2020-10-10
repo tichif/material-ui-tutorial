996e697f2750ff40b81726d0aa4e65cef2b73d37
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-10-09 22:14:45 -0400

    Work with Button and ButtonGroup Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
 import './App.css';
 
 function App() {
   return (
     <div>
-     <h1>Hello World</h1>
+     <ButtonGroup>
+     <Button startIcon={<SaveIcon />}  variant="contained" color="primary">
+       Save
+     </Button>
+     <Button startIcon={<DeleteIcon />}  variant="contained" color="secondary">
+       Discard
+     </Button>
+     </ButtonGroup>
     </div>
   );
 }
